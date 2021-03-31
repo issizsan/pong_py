d7a46da8f907cf01d97c5cef2eb473ff74feb882
ordaysan <cyneric18@protonmail.com>
2021-03-31 21:17:17 +0500

    Cleaned code, added collision

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-import sys, pygame
+import sys, pygame, random
+from pygame.locals import *
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 640, 480
+halfwidth = width / 2
+halfheight = height / 2
+
 speed = [2, 2]
 black = 0,0,0
 white = (255, 255, 255)
 
 screen = pygame.display.set_mode(size)  
 
-"""
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
-"""
+def draw():
+    pygame.draw.rect(screen, white, player2)
+    pygame.draw.rect(screen, white, player1)
+    pygame.draw.rect(screen, white, ball, border_radius=20)
 
-pygame.draw.rect(screen, white, (10, ((240/2)-30), 10, 60))
-pygame.draw.circle(screen, white, (320/2, 240/2), 10)
-pygame.draw.rect(screen, white, (320-20, ((240/2)-30), 10, 60))
+bspeed_x = 7
+bspeed_y = 7
 
+movement = 'up'
 
+player1 = pygame.Rect(10, ((height/2)-60), 12, 100)
+player2 = pygame.Rect(width-22, ((height/2)-60), 12, 100)
+ball = pygame.Rect(halfwidth, halfheight, 25, 25)
+        
 while True:
+    screen.fill(black)
+    ball.x += bspeed_x
+    ball.y += bspeed_y
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_DOWN:
+                player1.y -= 5
 
-    """ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    if ball.left < 0 or ball.right > width:
+        bspeed_x *= -1
+    if ball.top < 0 or ball.bottom > height:
+        bspeed_y *= -1
     
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    """
-    pygame.display.flip()
+    if ball.colliderect(player1):
+        bspeed_x *= -1
+    if ball.colliderect(player2):
+        bspeed_x *= -1
+
+    draw()
+    pygame.display.update()
+    fpsClock.tick(FPS)
