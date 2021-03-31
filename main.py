0ab9ea60296c5e9cbc74d41a5e4d7f7bbe4a4bb8
ordaysan <cyneric18@protonmail.com>
2021-03-31 23:10:47 +0500

    Add paddle collision

--- main.py
+++ main.py
@@ -23,8 +23,12 @@ def draw():
 
 bspeed_x = 7
 bspeed_y = 7
+pspeed = 7
 
-movement = 'up'
+mv1_up = False 
+mv1_down = False
+mv2_up = False
+mv2_down = False 
 
 player1 = pygame.Rect(10, ((height/2)-60), 12, 100)
 player2 = pygame.Rect(width-22, ((height/2)-60), 12, 100)
@@ -35,11 +39,40 @@ while True:
     ball.x += bspeed_x
     ball.y += bspeed_y
 
+    if mv1_down:
+        player1.y += pspeed
+    elif mv1_up:
+        player1.y -= pspeed
+    
+    elif mv2_down:
+        player2.y += pspeed
+    elif mv2_up:
+        player2.y -= pspeed
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_DOWN:
-                player1.y -= 5
+            if event.key == pygame.K_s:
+                mv1_down = True
+            if event.key == pygame.K_w:
+                mv1_up = True
+            
+            if event.key == pygame.K_DOWN:
+                mv2_down = True
+            if event.key == pygame.K_UP:
+                mv2_up = True
+            
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                mv1_down = False  
+            if event.key == pygame.K_w:
+                mv1_up = False  
+            
+            if event.key == pygame.K_DOWN:
+                mv2_down = False  
+            if event.key == pygame.K_UP:
+                mv2_up = False  
+
 
     if ball.left < 0 or ball.right > width:
         bspeed_x *= -1
@@ -51,6 +84,16 @@ while True:
     if ball.colliderect(player2):
         bspeed_x *= -1
 
+    if player1.top < 0:
+        mv1_up = False
+    if player2.top < 0:
+        mv2_up = False
+    
+    if player1.bottom > height:
+        mv1_down = False
+    if player2.bottom > height:
+        mv2_down = False
+    
     draw()
     pygame.display.update()
     fpsClock.tick(FPS)
